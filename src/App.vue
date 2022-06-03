806f713cb2771e9ec0437bed9fb2762b81cff418
Eomjihye12 <ojawl12044@gmail.com>
2022-06-03 16:44:28 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.18/vue.min.js"></script>
+<script>
+export default {
+  name: 'OrderList',
+  data: function () {
+    return {
+      orderList: []
+    }
+  },
+  created: function () {
+    this.getList()
+  },
+  methods: {
+    getList: function () {
+      // axios를 이용하여 API 호출 (component 안에서 axios를 this.$axios로 사용할 수 있습니다.)
+      this.$axios.get('http://www.localhost:3000/order/orders').then(response => {
+        console.log('### response: ' + JSON.stringify(response))
+        this.OrderList = response.data
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
