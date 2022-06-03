15eb6af01e1409b65310c837c724bd5b24667b59
Eomjihye12 <ojawl12044@gmail.com>
2022-06-03 17:00:07 +0900

    버그 수정

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
