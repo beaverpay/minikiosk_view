97b7faff1ce879665c2f1abb106c182feb97cfa5
m16khb <m16khb@gmail.com>
2022-06-05 19:22:39 +0900

    부트스트랩 설치 / 컴포넌트 이름 설정 추가

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import axios from 'axios'
+import Vue from 'vue';
+import App from './App.vue';
+import axios from 'axios';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.prototype.$http = axios;
-Vue.prototype.$axios = axios
-Vue.config.productionTip = false
+Vue.prototype.$axios = axios;
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	render: (h) => h(App),
+}).$mount('#app');
