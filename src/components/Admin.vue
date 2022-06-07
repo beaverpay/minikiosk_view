fb47088f4bfe5811d4b31b5282a55200e42c85a3
m16khb <m16khb@gmail.com>
2022-06-07 22:24:22 +0900

    이름 변경

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="row">
 		<div class="col-6">
-			<h1>메뉴관리</h1>
+			<h1>매니저관리</h1>
 		</div>
 		<div class="col-6">
-			<h1>주문관리</h1>
+			<h1>매장관리</h1>
 		</div>
 	</div>
 </template>
