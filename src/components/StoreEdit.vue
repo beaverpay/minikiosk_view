6d3fa7ff3a4437d1ed42abfb22bdf9f5ccf1268e
Eomjihye12 <ojawl12044@gmail.com>
2022-06-08 17:58:32 +0900

    주문 css 살짝 수정

--- src/components/StoreEdit.vue
+++ src/components/StoreEdit.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style ="margin:210px">
+    <div style ="margin:250px">
         <div align="right">
             <button type="button" class="btn btn-outline-info btn-sm" @click='confirmWrite()'>주문하기</button>&nbsp;
             <button type="button" class="btn btn-outline-secondary btn-sm" @click='cancelWrite()' >취소</button> 
