fbb8d595b03263afc4aeb92baed5531da288b3c4
Eomjihye12 <ojawl12044@gmail.com>
2022-06-08 10:51:47 +0900

    주문 수정

--- src/components/StoreEdit.vue
+++ src/components/StoreEdit.vue
@@ -4,10 +4,6 @@
             <button type="button" class="btn btn-outline-info btn-sm" @click='confirmWrite()'>주문하기</button>&nbsp;
             <button type="button" class="btn btn-outline-secondary btn-sm" @click='cancelWrite()' >취소</button> 
         </div>
-         <div class="mb-3" align="left">
-            <label for="exampleFormControlInput1" class="form-label">메뉴번호</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" :disabled="this.editMode" v-model="menu_id" >
-        </div>
         <div class="mb-3" align="left">
             <label for="exampleFormControlInput1" class="form-label">수량</label>
             <input type="email" class="form-control" id="exampleFormControlInput1" v-model= "order_amount">
@@ -20,6 +16,7 @@ export default {
     name: 'storeEdit',
     data() {
         return {
+            menu_store_id: null,
             menu_id: null,
             order_amount: null
         }  
@@ -36,6 +33,8 @@ export default {
         confirmWrite(){
                 //전송 데이터 셋팅
                 axios.post('http://ec2-3-36-49-133.ap-northeast-2.compute.amazonaws.com/order', {
+                    //매장번호
+                    menu_store_id: this.menu_store_id,
                     //메뉴번호
                     menu_id: this.menu_id,
                     //수량 
@@ -59,6 +58,7 @@ export default {
 
     }, 
     created(){
+        this.menu_store_id = this.$route.query.id;
         this.menu_id = this.$route.query.menu_id;
     }
 }
